1b577eea1624de23034bdd2045ffa11c8c6fdcc5
hdubey64 <hdubey64@gmail.com>
2022-09-09 16:53:31 +0530

    .startsWith Function

--- TestingIfAStringContainsASubstring.js
+++ TestingIfAStringContainsASubstring.js
@@ -1,7 +1,16 @@
 const string = "This is my string";
 
+// .includes Function
+
 if (string.includes("This")) {
    console.log("Found :- This !");
 } else {
    console.log("No :- This here !");
+}
+//// .startsWith Function
+
+if (string.startsWith("my")) {
+   console.log("Found :- My !");
+} else {
+   console.log("No :- My here !");
 }
